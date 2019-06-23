d6f183de809daf2a054fecb6c395c61dcd1f2e1d
javiferran <javiferran@hotmail.com>
2019-06-23 19:35:00 +0200

    Final code

--- model_prep.R
+++ model_prep.R
@@ -74,6 +74,9 @@ test_outcome_tournament <- outcome_tournament %>% filter(season > 2013) #Test sa
 #validation has target and Pred for every game that occurered 2014-2018
 #Apply LogLoss to validation$Pred and validation$team1win
 
+train$elo_diff <- NULL
+
+train$diff_rank <- NULL
 
 #Regularized Logistic Regression
 training_set <- train[,-(1:4)]
@@ -81,25 +84,27 @@ col_order <- colnames(training_set)
 
 test_set<- d_ss[,-1][,-(2:4)]
 colnames(test_set)[1] <- "team1win"
+test_set$elo_diff <- NULL
+test_set$diff_rank <- NULL
 test_set <- test_set[, col_order]
 
-######################################################################################
 
+
+######################################################################################
 #logistic regression model: differences
+
 model <- glm(team1win ~ .
              ,
              
              data = training_set, family = binomial)
 
-
+summary(model)
 #Predict on every possible matchup
 predict <- data.frame(Pred = predict(model, newdata = test_set, type = 'response'))
 d_ss <- d_ss %>% mutate(Pred = predict$Pred)# %>% dplyr::select(ID, Pred) Change sample submission pred=0.5 to model predicition
 
 d_ss_fin <- sample_submission %>% mutate(Pred = d_ss$Pred) #only matchup and prediction -> Results for kaggle
 #write.csv(d_ss_fin, "submission_stage_2.csv", row.names = FALSE)
-
-summary(model)
 
 ######################################################################################
 #Model test
@@ -113,6 +118,43 @@ LogLoss(y_pred = test_result$Pred, y_true = test_result$team1win)
 #Accuracy(y_pred = test_result$Pred, y_true = test_result$team1win)
 
 ######################################################################################
+# Neural network
+library(neuralnet)
+
+
+train_nnet_scaled <- as.data.frame(scale(training_set[-1]))
+train_nnet_scaled <- cbind(training_set$team1win,train_nnet_scaled)
+colnames(train_nnet_scaled)[1] <- "team1win"
+
+
+names <- colnames(train_nnet_scaled)[-1] #choose the names you want
+a <- as.formula(paste('team1win ~ ' ,paste(names,collapse='+')))
+a
+#neuralnet DOESN'T need factors as target
+nn <- neuralnet(a, data=train_nnet_scaled, hidden=c(1), linear.output=FALSE, threshold=0.01)
+nn$result.matrix
+plot(nn)
+
+test_set_scaled <- scale(test_set)
+
+test_set_scaled <- subset(test_set_scaled, select = colnames(test_set_scaled)[-1])
+nn.results <- compute(nn, test_set_scaled)
+
+d_ss <- d_ss %>% mutate(Pred = as.numeric(nn.results$net.result))
+
+######################################################################################
+#Model test
+test_result <- merge(x = test_outcome_tournament, y = d_ss[2:5], by=c("team1id","team2id","season"), all = FALSE)
+
+LogLoss(y_pred = test_result$Pred, y_true = test_result$team1win)
+
+# if Accuracy
+#test_result$Pred<- factor(test_result$Pred, labels=c(0,1))
+#Accuracy(y_pred = test_result$Pred, y_true = test_result$team1win)
+
+######################################################################################
+
+
 #Decision tree
 library(rpart)
 train_tree <- training_set
@@ -172,6 +214,9 @@ test_set_rf <- test_set
 test_set_rf$team1win <- NULL
 test_set_rf$team1win <- sample(c(0, 1), nrow(test_set_rf), replace=TRUE)
 
+colnames(test_set_rf)
+colnames(train_tree)
+
 test_set_rf <- test_set_rf[, col_order]
 test_set_rf$team1win<- factor(test_set_rf$team1win, labels=c(0,1))
 
@@ -189,10 +234,10 @@ random_forest <- randomForest(formula = team1win ~.,
                               norm.votes=T,
                               keep.forest=TRUE)
 
-#rf_predictions_prob <- predict(random_forest, test_set_rf, type='prob')
-rf_predictions_class <- predict(random_forest, test_set_rf, type='class')
-#d_ss <- d_ss %>% mutate(Pred = rf_predictions_prob[,2])#For prob
-d_ss <- d_ss %>% mutate(Pred = rf_predictions_class)
+rf_predictions_prob <- predict(random_forest, test_set_rf, type='prob')
+#rf_predictions_class <- predict(random_forest, test_set_rf, type='class')
+d_ss <- d_ss %>% mutate(Pred = rf_predictions_prob[,2])#For prob
+#d_ss <- d_ss %>% mutate(Pred = rf_predictions_class)
 
 ######################################################################################
 #Model test
@@ -210,7 +255,7 @@ LogLoss(y_pred = test_result$Pred, y_true = test_result$team1win)
 library(caret)
 library(caretEnsemble)
 library(knitr)
-train_ensemble <- train_tree
+train_ensemble <- training_set
 train_ensemble$diff_rank <- NULL
 train_ensemble$elo_diff <- NULL
 train_ensemble$team1win<- factor(train_ensemble$team1win, labels=c("win","loss"))#not for neuralnet
@@ -245,9 +290,8 @@ test_result <- merge(x = test_outcome_tournament, y = d_ss[2:5], by=c("team1id",
 
 LogLoss(y_pred = test_result$Pred, y_true = test_result$team1win)
 
-# if Accuracy
-#test_result$Pred<- factor(test_result$Pred, labels=c(0,1))
-#Accuracy(y_pred = test_result$Pred, y_true = test_result$team1win)
+d_ss_fin <- sample_submission %>% mutate(Pred = d_ss$Pred) #only matchup and prediction -> Results for kaggle
+write.csv(d_ss_fin, "submission_stage_2.csv", row.names = FALSE)
 
 
 
